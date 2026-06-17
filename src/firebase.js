4a61e4484ef5041398c4ae95f58b8e69e0933daf
Ragavi Jagdish <ragavij@Ragavis-MacBook-Air.local>
2026-06-17 22:53:50 +0530

    feat: integrate Firebase Analytics SDK

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getFunctions } from 'firebase/functions';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBEDXyQzVaK_tcbnGFBeoFIPDkH8K381CI",
@@ -9,11 +10,13 @@ const firebaseConfig = {
   projectId: "todfeed-app-61326",
   storageBucket: "todfeed-app-61326.firebasestorage.app",
   messagingSenderId: "921788139665",
-  appId: "1:921788139665:web:c19e89c59dfdabaf291730"
+  appId: "1:921788139665:web:c19e89c59dfdabaf291730",
+  measurementId: "G-JR9DCTXEEZ"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const functions = getFunctions(app);
+export const analytics = getAnalytics(app);
 export const googleProvider = new GoogleAuthProvider();
